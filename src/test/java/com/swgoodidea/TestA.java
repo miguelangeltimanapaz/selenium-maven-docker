ef9b584e0d5bb889ec203e3a761720c7ec7d8a6f
Miguel Ángel Timanaá Paz <miguel.angel.timana.paz@everis.com>
2021-07-21 01:10:02 -0500

    versión inicial

---
+++ src/test/java/com/swgoodidea/TestA.java
@@ -0,0 +1,41 @@
+package com.swgoodidea;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.safari.SafariOptions;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+import org.testng.Assert;
+import java.net.MalformedURLException;
+
+public class TestA {
+
+	//protected static ThreadLocal<RemoteWebDriver> driver = new ThreadLocal<>();
+	private static RemoteWebDriver webdriver = null;
+	public static String remote_url_chrome = "http://localhost:4445/wd/hub";
+
+	@BeforeClass
+	public void setUp() throws MalformedURLException {
+		ChromeOptions options = new ChromeOptions();
+        webdriver = new RemoteWebDriver(new URL(remote_url_chrome), options);
+		System.out.println("*******************");
+		System.out.println("launching browser");
+
+	}
+
+	@Test
+	public void testGooglePageTitle() {
+		//WebDriver webdriver = driver.get();
+		webdriver.navigate().to("https://www.google.com");
+		String strPageTitle = webdriver.getTitle();
+		System.out.println("Título de la página: - "+strPageTitle);
+		Assert.assertTrue(strPageTitle.equalsIgnoreCase("Google Test"), "Página del título no coincide");
+	}
+}
